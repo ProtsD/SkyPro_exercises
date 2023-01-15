308f8d9dbc37ffd297be7a9bebdbc4d64423a904
Denis <protsdv@gmail.com>
2023-01-15 18:37:14 +0300

    HW1 is done

---
+++ src/com/skypro/course2/HW1_IntrouctionOOP/Main.java
@@ -0,0 +1,70 @@
+package com.skypro.course2.HW1_IntrouctionOOP;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car lada = new Car(
+                "Lada",
+                "Granta",
+                1.7,
+                "yellow",
+                2015,
+                "Russia");
+
+        Car audi = new Car(
+                "Audi",
+                "A8 50 L TDI quattro",
+                3.0, "black",
+                2020,
+                "Germany");
+
+        Car bMW = new Car(
+                "BMW",
+                "Z8",
+                3.0,
+                "black",
+                2021,
+                "Germany");
+
+        Car kia = new Car(
+                "Kia",
+                "Sportage 4th generation",
+                2.4,
+                "red",
+                2018,
+                "South Korea");
+
+        Car hyundai = new Car(
+                "Hyundai",
+                "Avante",
+                1.6,
+                "orange",
+                2016,
+                "South Korea");
+
+        System.out.println(lada);
+        System.out.println(audi);
+        System.out.println(bMW);
+        System.out.println(kia);
+        System.out.println(hyundai);
+
+        Car hyundai2 = new Car(
+                "",
+                "",
+                0,
+                "",
+                0,
+                "");
+
+        Car hyundai3 = new Car(
+                null,
+                null,
+                -1,
+                null,
+                -1,
+                null);
+
+        System.out.println(hyundai2);
+        System.out.println(hyundai3);
+    }
+}
